0a3d7bf5f6d52c7326caec402287002e632c7aec
JUYEONCODE <joseph.oh0312@gmail.com>
2022-05-12 15:18:51 +0900

    commit

--- thirdapp/models.py
+++ thirdapp/models.py
@@ -1,5 +1,8 @@
+from turtle import distance
 from django.db import models
 from django.db.models.fields import CharField, IntegerField
+from django.forms import DateField
+from django.forms import FloatField
 
 class Shop(models.Model):
     shop_id = IntegerField(primary_key=True)
@@ -10,5 +13,17 @@ class Shop(models.Model):
     img_path = CharField(max_length=255, null=True)
     class Meta:
         db_table = 'shop'
+        app_label = 'thirdapp'
+        managed = False
+
+class JejuOlle(models.Model):
+    course = CharField(max_length=10)
+    course_name = CharField(max_length=20)
+    distance = FloatField()
+    time_info = CharField(max_length=10)
+    start_end_info = CharField(max_length=30)
+    cre_date = DateField()
+    class Meta:
+        db_table = 'jeju_olle'
         app_label = 'thirdapp'
         managed = False
--- thirdapp/views.py
+++ thirdapp/views.py
@@ -1,5 +1,6 @@
+from urllib import request
 from django.shortcuts import render
-from .models import Shop
+from .models import Shop,JejuOlle
 
 def shop(request):
     shop_list = Shop.objects.all()
@@ -8,4 +9,13 @@ def shop(request):
         request,
         'thirdapp/shop.html',
         {'shop_list': shop_list}
+)
+
+def jeju_olle(request):
+    jeju_olle_list = JejuOlle.objects.all()
+    
+    return render(
+        request,
+        'thirdapp/jeju_olle.html',
+        {'jeju_olle_list': jeju_olle_list}
 )
