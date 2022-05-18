fc531983693261c9700d4628da7d0d27b02cb628
JUYEONCODE <joseph.oh0312@gmail.com>
2022-05-18 15:44:21 +0900

    commt

--- file/views.py
+++ file/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import UploadFileForm
 
+
+def upload3(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            uploadFile = form.save()# uploadFile = form.save(commit=False)
+            name = uploadFile.file.name
+            size = uploadFile.file.size
+        return HttpResponse('%s<br>%s' % (name, size))
+    else:
+        form = UploadFileForm()
+    return render(
+        request, 'file/upload3.html', {'form': form})
 
 def upload1(request):
     if request.method == 'POST':
@@ -26,3 +40,16 @@ def upload2(request):
                     result += '%s<br>%s<hr>' % (name, size)
         return HttpResponse(result)
     return render(request, 'file/upload2.html')
+
+def upload3(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            uploadFile = form.save()# uploadFile = form.save(commit=False)
+            name = uploadFile.file.name
+            size = uploadFile.file.size
+        return HttpResponse('%s<br>%s' % (name, size))
+    else:
+        form = UploadFileForm()
+    return render(
+        request, 'file/upload3.html', {'form': form})
